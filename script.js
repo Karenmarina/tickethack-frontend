1eb625f70c9220e7805ec3beef73ea0964916973
Elisa <elisafan8@gmail.com>
2024-07-02 23:25:52 +0200

    v0 integrationhtml page cart + fetch Search mock data

---
+++ script.js
@@ -0,0 +1,56 @@
+// Get the list of trips' result and display it
+document.querySelector("#btn-search").addEventListener("click", function () {
+  // retrieve search values into an objet
+  const departure = document.querySelector("#trip-departure").value;
+  const arrival = document.querySelector("#trip-arrival").value;
+  const date = document.querySelector("#trip-date").value;
+  console.log(departure, arrival, date);
+
+  //fetch list of results with input values in body
+  fetch("http://localhost:3000/trips", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ departure, arrival, date }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result) {
+        // add trip results in HTML
+        for (let i = 0; i < data.allTrips[0].length; i++) {
+          document.querySelector("#tripResults").innerHTML = `
+          <div class="tripContainer">
+            <p><span id="departure">${departure}</span>><span id="arrival">${arrival}</span>
+            </p>
+            <p id="date">${date}</p>
+            <p id="price">${price}€</p>
+            <button type="button" id="btn-book">Book</button>
+            </div>`;
+        }
+      } else {
+        document.querySelector("#tripResults").innerHTML = `
+        <img src="./images/notfound.png" id="train-logo" alt="" />
+            <p>No trip found</p>`;
+      }
+    });
+});
+
+// add a trip to the cart on-click Book
+for (let i = 0; i < document.querySelectorAll(".btn-book").length; i++) {
+  document
+    .querySelectorAll("#btn-book")
+    [i].addEventListener("click", function () {
+      const tripToBook = this.parentNode.children.textContent;
+
+      fetch("http://localhost:3000/cart", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ tripToBook }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.result) {
+            window.location.assign = "cart.html";
+          }
+        });
+    });
+}
