ce797e78ecaad632ed0b4b6b33537db265ca604b
Elisa <elisafan8@gmail.com>
2024-07-04 11:10:02 +0200

    last update before end hackathon

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.querySelector("#btn-search").addEventListener("click", function () {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.allTrips);
+      // console.log(data.allTrips);
       if (data.result) {
         // add trip results in HTML
         document.querySelector("#homepage-train").remove();
@@ -22,43 +22,72 @@ document.querySelector("#btn-search").addEventListener("click", function () {
         for (let i = 0; i < data.allTrips.length; i++) {
           //   console.log(data.allTrips[1]);
           //   console.log(data.allTrips[0][i].departure);
-          const formattedTime = moment(data.allTrips[i].date).format("HH:MM");
-          console.log(formattedTime);
+          const date = moment(data.allTrips[i].date).format("HH:MM");
+          // console.log(formattedTime);
           document.querySelector("#tripResults").innerHTML += `
           <div class="tripContainer">
             <p><span id="departure">${data.allTrips[i].departure}</span>><span id="arrival">${data.allTrips[i].arrival}</span>
             </p>
-            <p id="date">${formattedTime}</p>
+            <p id="date">${date}</p>
             <p id="price">${data.allTrips[i].price}€</p>
-            <button type="button" id="btn-book">Book</button>
-            </div>`;
+            </div>
+            <button type="button" id="btn-book">Book</button>`;
         }
       } else {
         document.querySelector("#tripResults").innerHTML = `
         <img src="./images/notfound.png" id="train-logo" alt="" />
             <p>No trip found</p>`;
       }
+      // click on Book button
+      console.log(document.querySelectorAll("#btn-book"));
+      for (let i = 0; i < document.querySelectorAll("#btn-book").length; i++) {
+        document
+          .querySelectorAll("#btn-book")
+          [i].addEventListener("click", function () {
+            const departure = data.allTrips[i].departure;
+            const arrival = data.allTrips[i].arrival;
+
+            const price = data.allTrips[i].price;
+            console.log({ departure, arrival, date, price });
+
+            fetch("http://localhost:3000/cart", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                departure,
+                arrival,
+                date,
+                price,
+              }),
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data);
+                if (data.result) {
+                  window.location.href = "cart.html";
+                }
+              });
+          });
+      }
     });
 });
 
+// document.querySelector("#btn-search").addEventListener("click", function () {
+//   // const tripToBook = this.parentNode.children.textContent;
+//   console.log("click detected");
+
+//   fetch("http://localhost:3000/cart", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify("click detected"),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.result) {
+//         window.location.assign = "cart.html";
+//       }
+//     });
+// });
+
 // add a trip to the cart on-click Book
 // CODE PAS PROPRE A REPRENDRE !!!
-for (let i = 0; i < document.querySelectorAll(".btn-book").length; i++) {
-  document
-    .querySelectorAll("#btn-book")
-    [i].addEventListener("click", function () {
-      const tripToBook = this.parentNode.children.textContent;
-
-      fetch("http://localhost:3000/cart", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ tripToBook }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.result) {
-            window.location.assign = "cart.html";
-          }
-        });
-    });
-}
